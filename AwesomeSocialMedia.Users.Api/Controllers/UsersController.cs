e871ddb6a43ef9e6ed66c9804a122464c4db3079
João Carrijo <joao.carrijo@luby.com.br>
2023-11-25 14:14:25 -0300

    Finalização endpoint de usuários

--- AwesomeSocialMedia.Users.Api/Controllers/UsersController.cs
+++ AwesomeSocialMedia.Users.Api/Controllers/UsersController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AwesomeSocialMedia.Users.Application.Commands.SignUpUser;
+using AwesomeSocialMedia.Users.Application.Commands.UpdateUser;
+using AwesomeSocialMedia.Users.Application.Queries.GetUserById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AwesomeSocialMedia.Users.Api.Controllers
 {
@@ -6,6 +10,34 @@ namespace AwesomeSocialMedia.Users.Api.Controllers
     [Route("api/users")]
     public class UsersController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var query = new GetUserByIdQuery(id);
+
+            return Ok(await _mediator.Send(query));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(SignUpUserCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetById), new { id = result.Data }, command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id, UpdateUserCommand command)
+        {
+            command.Id = id;
+
+            return Ok(await _mediator.Send(command));
+        }
     }
 }
